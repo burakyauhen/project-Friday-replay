8de95a143b9010428abebd2fdd3cf2cc927e015a
Burak Yauhen <burakyauhen@gmail.com>
2023-08-10 11:41:49 +0300

    feat: add project build

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import './main.scss';
+import './fonts/fonts.scss';
+
---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.ts'),
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/, 
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            browsers: 'last 2 versions',
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,               
+                type: 'asset/resource',
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('icons', '[name].[contenthash:8][ext]'),
+                },
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('[name].[contenthash:8][ext]'),
+                },
+            },
+        ],
+    },
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.[contenthash:8].js',
+        assetModuleFilename: path.join('images', '[name].[contenthash:8][ext]'),
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'template.html'),
+            filename: 'index.html',
+        }),
+
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['dist'],
+                },
+            },
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash:8].css',
+        }),     
+    ],
+
+    devServer: {
+        watchFiles: path.join(__dirname, 'src'),
+        port: 9000, 
+    },
+
+    optimization: {
+        minimizer: [
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        plugins: [
+                            ['gifsicle', { interlaced: true }],
+                            ['jpegtran', { progressive: true }],
+                            ['optipng', { optimizationLevel: 5 }],
+                            ['svgo', { name: 'preset-default' }],
+                        ],
+                    },
+                },
+            }),
+        ],
+    },
+}
